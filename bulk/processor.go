c7e9aa5c5dd1aaa86823765c579ed3ce11b12f34
Michael Fraenkel <fraenkel@us.ibm.com>
2014-11-12 15:57:29 -0800

    Set up the Transport properly for timeouts and SSL
    [#82411300]

--- bulk/processor.go
+++ bulk/processor.go
@@ -2,6 +2,7 @@ package bulk
 
 import (
 	"crypto/tls"
+	"net"
 	"net/http"
 	"os"
 	"time"
@@ -97,8 +98,15 @@ func (p *Processor) sync(signals <-chan os.Signal) bool {
 	httpClient := &http.Client{
 		Timeout: p.ccFetchTimeout,
 		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			Dial: (&net.Dialer{
+				Timeout:   10 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 10 * time.Second,
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: p.skipCertVerify,
+				MinVersion:         tls.VersionTLS10,
 			},
 		},
 	}
