406537f6c4cc15e9617f1288ac22e7901c1a41d2
Luan Santos <lsantos@pivotal.io>
2016-03-24 17:00:03 -0700

    Stop using real BBS in nsync-listener tests
    [#113941499]
    
    Signed-off-by: Dan Wendorf <dwendorf@pivotal.io>

--- cmd/nsync-listener/main_suite_test.go
+++ cmd/nsync-listener/main_suite_test.go
@@ -2,37 +2,25 @@ package main_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/url"
-	"strings"
 	"testing"
 
-	"github.com/cloudfoundry-incubator/bbs"
-	bbstestrunner "github.com/cloudfoundry-incubator/bbs/cmd/bbs/testrunner"
 	"github.com/cloudfoundry-incubator/consuladapter"
 	"github.com/cloudfoundry-incubator/consuladapter/consulrunner"
-	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-	"github.com/tedsuo/ifrit"
-	"github.com/tedsuo/ifrit/ginkgomon"
+	"github.com/onsi/gomega/ghttp"
 )
 
 var (
 	listenerPath string
 
-	bbsPath   string
-	bbsURL    *url.URL
-	bbsClient bbs.Client
+	bbsURL  *url.URL
+	fakeBBS *ghttp.Server
 )
 
-var bbsArgs bbstestrunner.Args
-var bbsRunner *ginkgomon.Runner
-var bbsProcess ifrit.Process
-
-var etcdRunner *etcdstorerunner.ETCDClusterRunner
 var natsPort int
 
 var consulRunner *consulrunner.ClusterRunner
@@ -47,12 +35,8 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	listener, err := gexec.Build("github.com/cloudfoundry-incubator/nsync/cmd/nsync-listener", "-race")
 	Expect(err).NotTo(HaveOccurred())
 
-	bbs, err := gexec.Build("github.com/cloudfoundry-incubator/bbs/cmd/bbs", "-race")
-	Expect(err).NotTo(HaveOccurred())
-
 	payload, err := json.Marshal(map[string]string{
 		"listener": listener,
-		"bbs":      bbs,
 	})
 	Expect(err).NotTo(HaveOccurred())
 
@@ -66,50 +50,24 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	listenerPath = string(binaries["listener"])
 
 	natsPort = 4001 + GinkgoParallelNode()
-	etcdPort := 5001 + GinkgoParallelNode()
-
-	etcdRunner = etcdstorerunner.NewETCDClusterRunner(etcdPort, 1, nil)
 
 	consulRunner = consulrunner.NewClusterRunner(
 		9001+config.GinkgoConfig.ParallelNode*consulrunner.PortOffsetLength,
 		1,
 		"http",
 	)
-
-	bbsPath = string(binaries["bbs"])
-	bbsAddress := fmt.Sprintf("127.0.0.1:%d", 13000+GinkgoParallelNode())
-
-	bbsURL = &url.URL{
-		Scheme: "http",
-		Host:   bbsAddress,
-	}
-
-	bbsArgs = bbstestrunner.Args{
-		Address:           bbsAddress,
-		AdvertiseURL:      bbsURL.String(),
-		AuctioneerAddress: "some-address",
-		EtcdCluster:       strings.Join(etcdRunner.NodeURLS(), ","),
-		ConsulCluster:     consulRunner.ConsulCluster(),
-
-		EncryptionKeys: []string{"label:key"},
-		ActiveKeyLabel: "label",
-	}
 })
 
 var _ = BeforeEach(func() {
-	etcdRunner.Start()
 	consulRunner.Start()
 	consulRunner.WaitUntilReady()
 	consulClient = consulRunner.NewClient()
 
-	bbsRunner = bbstestrunner.New(bbsPath, bbsArgs)
-	bbsProcess = ginkgomon.Invoke(bbsRunner)
-	bbsClient = bbs.NewClient(bbsURL.String())
+	fakeBBS = ghttp.NewServer()
+	fakeBBS.AllowUnhandledRequests = false
 })
 
 var _ = AfterEach(func() {
-	ginkgomon.Kill(bbsProcess)
-	etcdRunner.Stop()
 	consulRunner.Stop()
 })
 
